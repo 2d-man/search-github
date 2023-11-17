849d8e6d4fe9208c6a9e7fa898832fcfbb8f8555
2d-man <v.belyakov1997@gmail.com>
2023-11-17 19:11:23 +0300

    feat: add SvgNotFound, SvgSearch

---
+++ src/assets/SvgNotFound.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style="margin-right:-1.6px;display:block;animation-play-state:paused"><rect x="8.5" y="8.5" width="20" height="20" fill="rgb(187, 74, 87)" style="animation-play-state:paused" />
+    <rect x="29.5" y="8.5" width="20" height="20" fill="rgb(187, 74, 87)" style="animation-play-state:paused" />
+    <rect x="50.5" y="8.5" width="20" height="20" fill="rgb(187, 74, 87)" style="animation-play-state:paused" />
+    <rect x="71.5" y="8.5" width="20" height="20" fill="rgb(187, 74, 87)" style="animation-play-state:paused" />
+    <rect x="8.5" y="29.5" width="20" height="20" fill="rgb(187, 74, 87)" style="animation-play-state:paused" />
+    <rect x="71.5" y="29.5" width="20" height="20" fill="rgb(187, 74, 87)" style="animation-play-state:paused" />
+    <rect x="8.5" y="50.5" width="20" height="20" fill="rgb(187, 74, 87)" style="animation-play-state:paused" />
+    <rect x="71.5" y="50.5" width="20" height="20" fill="rgb(187, 74, 87)" style="animation-play-state:paused" />
+    <rect x="8.5" y="71.5" width="20" height="20" fill="rgb(187, 74, 87)" style="animation-play-state:paused" />
+    <rect x="29.5" y="71.5" width="20" height="20" fill="rgb(187, 74, 87)" style="animation-play-state:paused" />
+    <rect x="50.5" y="71.5" width="20" height="20" fill="rgb(187, 74, 87)" style="animation-play-state:paused" />
+    <rect x="71.5" y="71.5" width="20" height="20" fill="rgb(187, 74, 87)" style="animation-play-state:paused" /></svg>
+</template>
